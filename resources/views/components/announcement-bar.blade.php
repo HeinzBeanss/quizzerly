277629c6db2de9386ffb55c529d3abceed085421
David Bean <heinzbeansss@gmail.com>
2024-02-24 00:48:13 +0000

    Announcement bar styling update.

--- resources/views/components/announcement-bar.blade.php
+++ resources/views/components/announcement-bar.blade.php
@@ -1,5 +1,5 @@
 @guest
-    <div class="w-full text-faintest h-6 md:h-7 text-center text-xs font-thin md:font-normal md:text-sm pt-1">
+    <div class="w-full bg-background text-faintest h-6 md:h-7 text-center text-xs font-thin md:font-normal md:text-sm pt-1">
         Make sure you <a class="underline font-semibold" href="/register">log in</a> to create your own quizzes!
     </div>
 @endguest
